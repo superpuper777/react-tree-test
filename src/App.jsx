8d20acb0bb8e007d602e3f77616bd2f17cff4359
superpuper777 <superfevrik@gmail.com>
2025-03-15 22:41:25 +0300

    feat: add api and create tree

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+// import { initializeApi } from 'fx-react';
+import { v4 as uuidv4 } from 'uuid';
+// import api from './api';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [treeData, setTreeData] = useState(null);
+  const [treeName, setTreeName] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  const generateTreeName = () => {
+    const newTreeName = uuidv4();
+    localStorage.setItem('treeName', newTreeName);
+    return newTreeName;
+  };
+
+  const loadTree = async () => {
+    try {
+      const savedTreeName =
+        localStorage.getItem('treeName') || generateTreeName();
+      console.log('Tree Name:', savedTreeName);
+
+      setTreeName(savedTreeName);
+
+      const response = await fetch(
+        `https://test.vmarmysh.com/api.user.tree.get?treeName=${savedTreeName}`
+      );
+      // const response = await api.user.tree.get('treeAl');
+      const data = await response.json();
+      console.log('tree:', data);
+
+      setTreeData(data);
+    } catch (error) {
+      console.error('Error loading tree:', error);
+      setError('Error loading data');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadTree();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <p>Tree: {treeName}</p>
+      <p>{JSON.stringify(treeData, null, 2)}</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
